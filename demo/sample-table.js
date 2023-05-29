343ae8afbd1e2df2347956a282e60f5b459a70c9
suzzit <bajagainsuzzit@gmail.com>
2023-05-29 11:23:10 +0545

    sample-table -- eslint rules

--- demo/sample-table.js
+++ demo/sample-table.js
@@ -1,281 +1,281 @@
-var stock_1 = [
+let stock_1 = [
     {
-        "Name": "ADBL (Agriculture Development Bank Limited)",
-        "Stock No": "1",
-        "Symbol": "ADBL"
+        'Name': 'ADBL (Agriculture Development Bank Limited)',
+        'Stock No': '1',
+        'Symbol': 'ADBL',
     },
-    { "Name": "CBL (Civil Bank Ltd)", "Stock No": "3", "Symbol": "CBL" },
+    { 'Name': 'CBL (Civil Bank Ltd)', 'Stock No': '3', 'Symbol': 'CBL' },
     {
-        "Name": "CTBNL (Commerz and Trust Bank Nepal Ltd.)",
-        "Stock No": "4",
-        "Symbol": "CTBNL"
-    },
-    {
-        "Name": "CZBIL (Citizen Bank International Limited)",
-        "Stock No": "5",
-        "Symbol": "CZBIL"
+        'Name': 'CTBNL (Commerz and Trust Bank Nepal Ltd.)',
+        'Stock No': '4',
+        'Symbol': 'CTBNL',
     },
     {
-        "Name": "NABILB2075 (Nabil Bank Limited Bond 2075)",
-        "Stock No": "261",
-        "Symbol": "NABILB2075"
+        'Name': 'CZBIL (Citizen Bank International Limited)',
+        'Stock No': '5',
+        'Symbol': 'CZBIL',
     },
     {
-        "Name": "NIBB2071 (Nepal Investment Bank Bond 2071)",
-        "Stock No": "262",
-        "Symbol": "NIBB2071"
+        'Name': 'NABILB2075 (Nabil Bank Limited Bond 2075)',
+        'Stock No': '261',
+        'Symbol': 'NABILB2075'
     },
     {
-        "Name": "NIBB2072 (Nepal Investment Bank Limited Bond 2072)",
-        "Stock No": "263",
-        "Symbol": "NIBB2072"
+        'Name': 'NIBB2071 (Nepal Investment Bank Bond 2071)',
+        'Stock No': '262',
+        'Symbol': 'NIBB2071'
     },
     {
-        "Name": "NIBB2075 (Nepal Investment Bank Limited Bond 2075)",
-        "Stock No": "264",
-        "Symbol": "NIBB2075"
+        'Name': 'NIBB2072 (Nepal Investment Bank Limited Bond 2072)',
+        'Stock No': '263',
+        'Symbol': 'NIBB2072'
     },
     {
-        "Name": "NIBB2077 (Nepal Investment Bank Bond 2077)",
-        "Stock No": "265",
-        "Symbol": "NIBB2077"
-    },
-    { "Name": "SBIB2078 (Nepal SBI Bank -2078)", "Stock No": "266", "Symbol": "SBIB2078" },
-    { "Name": "SBIB2079 (Nepal SBI Bank - 2079)", "Stock No": "267", "Symbol": "SBIB2079" },
-    { "Name": "SBL2072 (Siddhartha Bank-2072)", "Stock No": "268", "Symbol": "SBL2072" },
-    {
-        "Name": "SBLB2075 (Siddhartha Bank Ltd.-2075)",
-        "Stock No": "269",
-        "Symbol": "SBLB2075"
+        'Name': 'NIBB2075 (Nepal Investment Bank Limited Bond 2075)',
+        'Stock No': '264',
+        'Symbol': 'NIBB2075'
     },
     {
-        "Name": "SBLB2076 (Siddhartha Bank Ltd. Bond -2076)",
-        "Stock No": "270",
-        "Symbol": "SBLB2076"
+        'Name': 'NIBB2077 (Nepal Investment Bank Bond 2077)',
+        'Stock No': '265',
+        'Symbol': 'NIBB2077'
+    },
+    { 'Name': 'SBIB2078 (Nepal SBI Bank -2078)', 'Stock No': '266', 'Symbol': 'SBIB2078' },
+    { 'Name': 'SBIB2079 (Nepal SBI Bank - 2079)', 'Stock No': '267', 'Symbol': 'SBIB2079' },
+    { 'Name': 'SBL2072 (Siddhartha Bank-2072)', 'Stock No': '268', 'Symbol': 'SBL2072' },
+    {
+        'Name': 'SBLB2075 (Siddhartha Bank Ltd.-2075)',
+        'Stock No': '269',
+        'Symbol': 'SBLB2075'
     },
     {
-        "Name": "EBLCP (Everest Bank Limited Con. Pref.)",
-        "Stock No": "271",
-        "Symbol": "EBLCP"
+        'Name': 'SBLB2076 (Siddhartha Bank Ltd. Bond -2076)',
+        'Stock No': '270',
+        'Symbol': 'SBLB2076'
     },
     {
-        "Name": "ACEDPO (ACE Development Bank Limited Promoter Share)",
-        "Stock No": "274",
-        "Symbol": "ACEDPO"
+        'Name': 'EBLCP (Everest Bank Limited Con. Pref.)',
+        'Stock No': '271',
+        'Symbol': 'EBLCP'
     },
     {
-        "Name": "AEFLPO (Alpic Everest Finance Limited Promoter Share)",
-        "Stock No": "275",
-        "Symbol": "AEFLPO"
+        'Name': 'ACEDPO (ACE Development Bank Limited Promoter Share)',
+        'Stock No': '274',
+        'Symbol': 'ACEDPO'
     },
     {
-        "Name": "AICPO (Alliance Insurance Company Ltd. Promoter)",
-        "Stock No": "276",
-        "Symbol": "AICPO"
+        'Name': 'AEFLPO (Alpic Everest Finance Limited Promoter Share)',
+        'Stock No': '275',
+        'Symbol': 'AEFLPO'
     },
     {
-        "Name": "ALICLP (Asian Life Insurance Co. Ltd Promoter Share)",
-        "Stock No": "277",
-        "Symbol": "ALICLP"
+        'Name': 'AICPO (Alliance Insurance Company Ltd. Promoter)',
+        'Stock No': '276',
+        'Symbol': 'AICPO'
     },
     {
-        "Name": "AXISP (Axis Development Bank Limited Promoter Share)",
-        "Stock No": "278",
-        "Symbol": "AXISP"
+        'Name': 'ALICLP (Asian Life Insurance Co. Ltd Promoter Share)',
+        'Stock No': '277',
+        'Symbol': 'ALICLP'
     },
     {
-        "Name": "BBBLNP (Bagheshowari Development Bank Ltd. Promoer Share)",
-        "Stock No": "279",
-        "Symbol": "BBBLNP"
+        'Name': 'AXISP (Axis Development Bank Limited Promoter Share)',
+        'Stock No': '278',
+        'Symbol': 'AXISP'
     },
     {
-        "Name": "BBBLPO (Bhrikuti Bikash Bank Limited Promoter Share)",
-        "Stock No": "280",
-        "Symbol": "BBBLPO"
+        'Name': 'BBBLNP (Bagheshowari Development Bank Ltd. Promoer Share)',
+        'Stock No': '279',
+        'Symbol': 'BBBLNP'
     },
     {
-        "Name": "BFLPO (Butwal Finance Limited Promoter Share)",
-        "Stock No": "281",
-        "Symbol": "BFLPO"
+        'Name': 'BBBLPO (Bhrikuti Bikash Bank Limited Promoter Share)',
+        'Stock No': '280',
+        'Symbol': 'BBBLPO'
     },
     {
-        "Name": "BLDBLP (Biratlaxmi Bikas Bank Limited Promoter Share)",
-        "Stock No": "282",
-        "Symbol": "BLDBLP"
+        'Name': 'BFLPO (Butwal Finance Limited Promoter Share)',
+        'Stock No': '281',
+        'Symbol': 'BFLPO'
     },
     {
-        "Name": "CDBLPO (City Development Bank Ltd. Promoter)",
-        "Stock No": "284",
-        "Symbol": "CDBLPO"
+        'Name': 'BLDBLP (Biratlaxmi Bikas Bank Limited Promoter Share)',
+        'Stock No': '282',
+        'Symbol': 'BLDBLP'
     },
     {
-        "Name": "CEDBLP (Clean Energy Development Bank Ltd. Promoter Share)",
-        "Stock No": "285",
-        "Symbol": "CEDBLP"
+        'Name': 'CDBLPO (City Development Bank Ltd. Promoter)',
+        'Stock No': '284',
+        'Symbol': 'CDBLPO'
     },
     {
-        "Name": "CMBFLP (Capital Merchant Bank. & Fin. Promoter Share)",
-        "Stock No": "286",
-        "Symbol": "CMBFLP"
+        'Name': 'CEDBLP (Clean Energy Development Bank Ltd. Promoter Share)',
+        'Stock No': '285',
+        'Symbol': 'CEDBLP'
     },
     {
-        "Name": "CMBFPO (CMB Finance Ltd. Promoter Share)",
-        "Stock No": "287",
-        "Symbol": "CMBFPO"
+        'Name': 'CMBFLP (Capital Merchant Bank. & Fin. Promoter Share)',
+        'Stock No': '286',
+        'Symbol': 'CMBFLP'
     },
     {
-        "Name": "CMBSLp (Civil Merchant Bittiya Sanstha Limited Promoter Share)",
-        "Stock No": "288",
-        "Symbol": "CMBSLp"
+        'Name': 'CMBFPO (CMB Finance Ltd. Promoter Share)',
+        'Stock No': '287',
+        'Symbol': 'CMBFPO'
     },
     {
-        "Name": "CZBILP (Citizens Bank Internatioal Limited Promoter Share)",
-        "Stock No": "289",
-        "Symbol": "CZBILP"
+        'Name': 'CMBSLp (Civil Merchant Bittiya Sanstha Limited Promoter Share)',
+        'Stock No': '288',
+        'Symbol': 'CMBSLp'
     },
     {
-        "Name": "EBLPO (Everest Bank Limited Promoter Share)",
-        "Stock No": "290",
-        "Symbol": "EBLPO"
+        'Name': 'CZBILP (Citizens Bank Internatioal Limited Promoter Share)',
+        'Stock No': '289',
+        'Symbol': 'CZBILP'
     },
     {
-        "Name": "EFLPO (Everest Finance Limited Promoter Share)",
-        "Stock No": "291",
-        "Symbol": "EFLPO"
+        'Name': 'EBLPO (Everest Bank Limited Promoter Share)',
+        'Stock No': '290',
+        'Symbol': 'EBLPO'
     },
     {
-        "Name": "FFCLPO (Fewa Finance Company Limited Promoter Share)",
-        "Stock No": "292",
-        "Symbol": "FFCLPO"
+        'Name': 'EFLPO (Everest Finance Limited Promoter Share)',
+        'Stock No': '291',
+        'Symbol': 'EFLPO'
     },
     {
-        "Name": "GBIMEP (Global IME Bank Limited Promoter Share)",
-        "Stock No": "293",
-        "Symbol": "GBIMEP"
+        'Name': 'FFCLPO (Fewa Finance Company Limited Promoter Share)',
+        'Stock No': '292',
+        'Symbol': 'FFCLPO'
     },
     {
-        "Name": "GFCLPO (Goodwill Finance Company Limited Promoter Share)",
-        "Stock No": "294",
-        "Symbol": "GFCLPO"
+        'Name': 'GBIMEP (Global IME Bank Limited Promoter Share)',
+        'Stock No': '293',
+        'Symbol': 'GBIMEP'
     },
     {
-        "Name": "GFLKPO (Gorkha Finance Limited Promoter Share)",
-        "Stock No": "295",
-        "Symbol": "GFLKPO"
+        'Name': 'GFCLPO (Goodwill Finance Company Limited Promoter Share)',
+        'Stock No': '294',
+        'Symbol': 'GFCLPO'
     },
     {
-        "Name": "GFLPO (General Finance Limited Promoter Share)",
-        "Stock No": "296",
-        "Symbol": "GFLPO"
+        'Name': 'GFLKPO (Gorkha Finance Limited Promoter Share)',
+        'Stock No': '295',
+        'Symbol': 'GFLKPO'
     },
     {
-        "Name": "GRANDP (Grand Bank Limited Promoter Share)",
-        "Stock No": "297",
-        "Symbol": "GRANDP"
+        'Name': 'GFLPO (General Finance Limited Promoter Share)',
+        'Stock No': '296',
+        'Symbol': 'GFLPO'
     },
     {
-        "Name": "GSDBLP (Gaurishankar Development Bank Ltd. Promoter Share)",
-        "Stock No": "298",
-        "Symbol": "GSDBLP"
+        'Name': 'GRANDP (Grand Bank Limited Promoter Share)',
+        'Stock No': '297',
+        'Symbol': 'GRANDP'
     },
     {
-        "Name": "HBDLPO (H & B Development Bank Limited Promoter Share)",
-        "Stock No": "299",
-        "Symbol": "HBDLPO"
+        'Name': 'GSDBLP (Gaurishankar Development Bank Ltd. Promoter Share)',
+        'Stock No': '298',
+        'Symbol': 'GSDBLP'
     },
     {
-        "Name": "HBLPO (Himalayan Bank Limited Promoter Share)",
-        "Stock No": "300",
-        "Symbol": "HBLPO"
+        'Name': 'HBDLPO (H & B Development Bank Limited Promoter Share)',
+        'Stock No': '299',
+        'Symbol': 'HBDLPO'
     },
     {
-        "Name": "ICFCPO (ICFC Finance Limited Promotor Share)",
-        "Stock No": "301",
-        "Symbol": "ICFCPO"
+        'Name': 'HBLPO (Himalayan Bank Limited Promoter Share)',
+        'Stock No': '300',
+        'Symbol': 'HBLPO'
     },
     {
-        "Name": "IDBLPO (Infrastructure Development Bank Limited Promoter Share)",
-        "Stock No": "302",
-        "Symbol": "IDBLPO"
+        'Name': 'ICFCPO (ICFC Finance Limited Promotor Share)',
+        'Stock No': '301',
+        'Symbol': 'ICFCPO'
     },
     {
-        "Name": "IFILPO (Imperial Finance Ltd Promoter Share)",
-        "Stock No": "303",
-        "Symbol": "IFILPO"
+        'Name': 'IDBLPO (Infrastructure Development Bank Limited Promoter Share)',
+        'Stock No': '302',
+        'Symbol': 'IDBLPO'
     },
     {
-        "Name": "IMEFIP (IME Financial Institution Promoter Share)",
-        "Stock No": "304",
-        "Symbol": "IMEFIP"
+        'Name': 'IFILPO (Imperial Finance Ltd Promoter Share)',
+        'Stock No': '303',
+        'Symbol': 'IFILPO'
     },
     {
-        "Name": "JBBLPO (Jyoti Bikash Bank Ltd. Promoter Share)",
-        "Stock No": "305",
-        "Symbol": "JBBLPO"
+        'Name': 'IMEFIP (IME Financial Institution Promoter Share)',
+        'Stock No': '304',
+        'Symbol': 'IMEFIP'
     },
     {
-        "Name": "JFLPO (Janaki Finance Limited Promoter Share)",
-        "Stock No": "306",
-        "Symbol": "JFLPO"
-    },
-    { "Name": "KAFILP (Kaski Finanace Ltd. Promoter)", "Stock No": "307", "Symbol": "KAFILP" },
-    {
-        "Name": "KBBLPO (Kailash Bikas Bank Limited Promoter Share)",
-        "Stock No": "308",
-        "Symbol": "KBBLPO"
+        'Name': 'JBBLPO (Jyoti Bikash Bank Ltd. Promoter Share)',
+        'Stock No': '305',
+        'Symbol': 'JBBLPO'
     },
     {
-        "Name": "KBLPO (Kumari Bank Limited Promotor Share)",
-        "Stock No": "309",
-        "Symbol": "KBLPO"
+        'Name': 'JFLPO (Janaki Finance Limited Promoter Share)',
+        'Stock No': '306',
+        'Symbol': 'JFLPO'
+    },
+    { 'Name': 'KAFILP (Kaski Finanace Ltd. Promoter)', 'Stock No': '307', 'Symbol': 'KAFILP' },
+    {
+        'Name': 'KBBLPO (Kailash Bikas Bank Limited Promoter Share)',
+        'Stock No': '308',
+        'Symbol': 'KBBLPO'
     },
     {
-        "Name": "KDBLPO (Kasthamandap Dev. Bank Limited Promoter Share)",
-        "Stock No": "310",
-        "Symbol": "KDBLPO"
+        'Name': 'KBLPO (Kumari Bank Limited Promotor Share)',
+        'Stock No': '309',
+        'Symbol': 'KBLPO'
     },
     {
-        "Name": "KFLPO (Kathmandu Finance Ltd. Promoter Share)",
-        "Stock No": "311",
-        "Symbol": "KFLPO"
+        'Name': 'KDBLPO (Kasthamandap Dev. Bank Limited Promoter Share)',
+        'Stock No': '310',
+        'Symbol': 'KDBLPO'
     },
     {
-        "Name": "KISTPO (KIST Bank Limited Promoter Share)",
-        "Stock No": "312",
-        "Symbol": "KISTPO"
+        'Name': 'KFLPO (Kathmandu Finance Ltd. Promoter Share)',
+        'Stock No': '311',
+        'Symbol': 'KFLPO'
+    },
+    {
+        'Name': 'KISTPO (KIST Bank Limited Promoter Share)',
+        'Stock No': '312',
+        'Symbol': 'KISTPO'
     },
 ]
 
-//Get the headers from JSON data
-var headers = Object.keys(stock_1[0]);
+// Get the headers from JSON data
+let headers = Object.keys(stock_1[0])
 
-//Prepare html header
-var headerRowHTML = '<tr>';
-for (var i = 0; i < headers.length; i++) {
-    headerRowHTML += '<th> <a>' + headers[i] + '</a> </th>';
+// Prepare html header
+let headerRowHTML = '<tr>'
+for (let i = 0; i < headers.length; i++) {
+    headerRowHTML += '<th> <a>' + headers[i] + '</a> </th>'
 }
-headerRowHTML += '</tr>';
+headerRowHTML += '</tr>'
 
-//Prepare all the employee records as HTML
-var allRecordsHTML = '';
-for (var i = 0; i < stock_1.length; i++) {
+// Prepare all the employee records as HTML
+let allRecordsHTML = ''
+for (let i = 0; i < stock_1.length; i++) {
 
-    //Prepare html row
-    allRecordsHTML += '<tr>';
-    for (var j = 0; j < headers.length; j++) {
-        var header = headers[j];
-        allRecordsHTML += '<td>' + stock_1[i][header] + '</td>';
+    // Prepare html row
+    allRecordsHTML += '<tr>'
+    for (let j = 0; j < headers.length; j++) {
+        let header = headers[j]
+        allRecordsHTML += '<td>' + stock_1[i][header] + '</td>'
     }
-    allRecordsHTML += '</tr>';
+    allRecordsHTML += '</tr>'
 
 }
 
-//Append the table header and all records
-var table = document.getElementById("display_json_data");
-table.innerHTML = headerRowHTML + allRecordsHTML;
+// Append the table header and all records
+let table = document.getElementById('display_json_data')
+table.innerHTML = headerRowHTML + allRecordsHTML
 
 
-// Sample of Datatable Initilization
+//  Sample of Datatable Initilization
 let dt = new simpleDatatables.DataTable('#display_json_data', {
     headers: true,
     columns: [
@@ -283,13 +283,13 @@ let dt = new simpleDatatables.DataTable('#display_json_data', {
             select: 0,
             render: function (data, td, tr) {
                 return data
-            }
+            },
         },
         {
             select: 1,
             render: function (data, td, tr) {
                 return data
-            }
+            },
         },
     ],
 })
