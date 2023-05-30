39b8a96b40571b66f1b9db0c6bed043e871900cf
aryalawjju23 <aryalawjju23@gmail.com>
2023-05-30 15:56:53 +0545

    run - runfile for flask

---
+++ run.py
@@ -0,0 +1,60 @@
+#!./venv/bin/python
+import os
+
+import flask
+import jinja2
+import qdump
+
+app = flask.Flask(__name__)
+
+
+@app.get('/static/vendors/<path:filepath>')
+def vendors(filepath):
+    return flask.send_from_directory('./vendors', filepath)
+
+
+@app.get('/')
+def test():
+    path  = os.path.abspath('.' + '/vendors/announcements30.pickle')  # noqa:401
+    query = qdump.load(path)
+    print(query[0])
+    headers = [k for k,v in query[0].items()]
+    print(headers)
+    return flask.render_template(
+        '/home.djhtml',
+        query = query,
+        headers = headers
+    )
+
+
+
+@app.get('/table')
+def table():
+    path  = os.path.abspath('.' + '/vendors/announcements30.pickle')  # noqa:401
+    query = qdump.load(path)
+    print(query[0])
+    headers = [k for k,v in query[0].items()]
+    print(headers)
+    return flask.render_template(
+        '/table.djhtml',
+        query = query,
+        headers = headers
+    )
+
+
+@app.post('/t')
+def t():
+    path  = os.path.abspath('.' + '/vendors/announcements30.pickle')  # noqa:401
+    query = qdump.load(path)
+    print(query[0])
+    headers = [k for k,v in query[0].items()]
+    print(headers)
+    return flask.render_template(
+        '/t.djhtml',
+        query = query,
+        headers = headers
+    )
+
+if __name__ == '__main__':
+    app.jinja_env.undefined = jinja2.StrictUndefined
+    app.run('0.0.0.0', port=8000, debug=True)
