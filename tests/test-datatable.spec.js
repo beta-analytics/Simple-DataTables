2d42199bbc51470c9f4dfd11b9efa07e83487e9d
suzzit <bajagainsuzzit@gmail.com>
2023-06-27 17:50:20 +0545

    test-datatable -- test cases to test rendering of layout

--- tests/test-datatable.spec.js
+++ tests/test-datatable.spec.js
@@ -8,15 +8,19 @@ function locateElement(root, selector) {
 }
 
 test.beforeEach(async ({ page }) => {
-    page.goto(testConfigs.url)
+    await page.goto(testConfigs.url)
 })
 
 test.describe('match config with UI', async () => {
 
-    test('is searchbar placeholder right', async function ({ page }) {
+    test('is searchbar present when searchale is true and placeholder is correct', async function ({ page }) {
         let searchBar = await locateElement(page, '.dataTable-input')
-
-        await expect(await (searchBar.getAttribute('placeholder'))).toEqual(defaultConfig.labels.placeholder)
+        if (defaultConfig.searchable) {
+            await expect(searchBar).toBeAttached() // assert whether the element is visible
+            await expect(await (searchBar.getAttribute('placeholder'))).toEqual(defaultConfig.labels.placeholder) // to assert the placeholder
+        } else {
+            await expect(searchBar).not.toBeAttached() // assert the input is not in existence
+        }
     })
 
     // test('is perpage label right', async function ({ page }) {
@@ -32,12 +36,37 @@ test.describe('match config with UI', async () => {
 
     test('No entries found', async({ page }) => {
         let searchBar = await locateElement(page, '.dataTable-input')
-        await searchBar.fill('kdjfjie') // Intentionally wrong value so that search break
+        await searchBar.fill('wrong value') // Intentionally wrong value so that search breaks
         await searchBar.press('Enter')
         let emptyTable = await locateElement(page, '.dataTables-empty').textContent()
         await expect(emptyTable).toEqual(defaultConfig.labels.noRows)
     })
 
+    test('is initial rows equal to perpage', async({ page }) =>{
+        let tr = await page.$$('#table tbody tr')
 
+        await expect(tr.length).toEqual(defaultConfig.perPage)
+    })
+
+    test('is perpage options correct', async({ page }) => {
+        let options = await page.$$('.dataTable-selector option')
+
+        let arr = []
+
+        await options.forEach((option)=>{
+            arr.push(option.getAttribute('option'))
+        })
+
+        // await expect(arr).toEqual(defaultConfig.perPageSelect)
+    })
+
+    test('table top bar is rendering correctly', async({ page }) => {
+        let topbar = await page.locator('.dataTable-top')
+        let topLayout = defaultConfig.layout.top
+        let topLayoutArr = topLayout.replaceAll('}{', ' ').replace(/{|}/g, '').split(' ')
+        await expect(await locateElement(topbar, `#${topLayoutArr[0]}`)).toBeAttached()
+        await expect(await locateElement(topbar, `#${topLayoutArr[1]}`)).toBeAttached()
+        await expect(await locateElement(topbar, `#${topLayoutArr[2]}`)).toBeAttached()
+    })
 
 })
