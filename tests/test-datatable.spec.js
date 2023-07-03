f1dca194f0497af40d4946a31c769feb235bd08b
suzzit <bajagainsuzzit@gmail.com>
2023-07-03 15:14:20 +0545

    test-datatable -- removed useables

--- tests/test-datatable.spec.js
+++ tests/test-datatable.spec.js
@@ -3,160 +3,153 @@ import testConfigs from './testconfigs'
 import { defaultConfig } from '../../table/src/config'
 
 // function to locate elements dynamically using locator method
-let page
-let useables = {}
-async function locateElement(root, selector, name = null) {
-    if (null == name) {
-        return root.locator(selector)
-    }
-    useables[name] = root.locator(selector)
-    return useables[name]
+async function locateElement(root, selector) {
+    return root.locator(selector)
 }
 
-test.describe.serial('tests for topbar', async () => {
+test.describe('datatable test', async () => {
 
-    test.beforeAll(async ({ browser }) => {
-        let context = await browser.newContext()
-        page = await context.newPage()
-    })
+    let page
+    test.describe.serial('tests for topbar', async () => {
 
-    test.beforeEach(async () => {
-        await page.goto(testConfigs.url)
-    })
+        test.beforeAll(async ({ browser }) => {
+            let context = await browser.newContext()
+            page = await context.newPage()
+            await page.goto(testConfigs.url)
+        })
 
-    test.afterAll(async ({ browser }) => {
-        browser.close()
-    })
+        test.afterAll(async ({ browser }) => {
+            browser.close()
+        })
 
-    test('table top bar is only rendering when layout is present', async () => {
-        await locateElement(page, '.dataTable-top', 'topbar')
-        if (!defaultConfig.layout) {
-            await expect(useables.topbar).not.toBeAttached()
-        } else {
-            await expect(useables.topbar).not.toBeAttached()
-        }
-    })
-
-    test('table top bar is only rendering when layout.top is non empty', async () => {
-        await locateElement(page, '.dataTable-top', 'topbar')
-        if (defaultConfig.layout) {
-            if ('' !== defaultConfig.layout.top) {
-                await expect(useables.topbar).toBeAttached()
+        test('table top bar is only rendering when layout is present', async () => {
+            let topbar = await locateElement(page, '.dataTable-top')
+            if (!defaultConfig.layout) {
+                await expect(topbar).not.toBeAttached()
             } else {
-                await expect(useables.topbar).not.toBeAttached()
+                await expect(topbar).toBeAttached()
             }
-        }
+        })
+
+        test('table top bar is only rendering when layout.top is non empty', async () => {
+            let topbar = await locateElement(page, '.dataTable-top')
+            if (defaultConfig.layout) {
+                if ('' !== defaultConfig.layout.top) {
+                    await expect(topbar).toBeAttached()
+                } else {
+                    await expect(topbar).not.toBeAttached()
+                }
+            }
+        })
+
+        test('table top bar is rendering correct child element as passed with options', async () => {
+            let topbar = await locateElement(page, '.dataTable-top')
+
+            let topLayout = defaultConfig.layout.top
+            let topLayoutArr = topLayout.replaceAll('}{', ' ').replace(/{|}/g, '').split(' ')
+            for (let i = 0; i < topLayoutArr.length; i++) {
+                await expect(await locateElement(topbar, `[data-testid = ${topLayoutArr[i]}]`)).toBeAttached()
+            }
+        })
+
+        test('is searchbar present when searchale is true and placeholder is correct', async function () {
+            let searchBar = await locateElement(page, '.dataTable-input')
+            if (defaultConfig.searchable) {
+                await expect(searchBar).toBeAttached() // assert whether the element is visible
+                await expect(await (searchBar.getAttribute('placeholder'))).toEqual(defaultConfig.labels.placeholder) // to assert the placeholder
+            } else {
+                await expect(searchBar).not.toBeAttached() // assert the input is not in existence
+            }
+        })
+
     })
 
-    test('table top bar is rendering correct child element as passed with options', async () => {
-        await locateElement(page, '.dataTable-top', 'topbar')
+    test.describe.serial('tests for bottom bar', async ()=>{
 
-        let topLayout = defaultConfig.layout.top
-        let topLayoutArr = topLayout.replaceAll('}{', ' ').replace(/{|}/g, '').split(' ')
-        for (let i = 0; i < topLayoutArr.length; i++) {
-            await expect(await locateElement(useables.topbar, `[data-testid = ${topLayoutArr[i]}]`)).toBeAttached()
-        }
+        test.beforeAll(async ({ browser }) => {
+            let context = await browser.newContext()
+            page = await context.newPage()
+            await page.goto(testConfigs.url)
+        })
+
+        test.afterAll(async ({ browser }) => {
+            browser.close()
+        })
+
+
+        test('table bottom bar is only rendering when layout is present', async () => {
+            let bottombar = await locateElement(page, '.dataTable-bottom')
+            if (!defaultConfig.layout) {
+                await expect(bottombar).not.toBeAttached()
+            } else {
+                await expect(bottombar).toBeAttached()
+            }
+        })
+
+        test('table bottom bar is only rendering when layout.bottom is non empty', async () => {
+            let bottombar = await locateElement(page, '.dataTable-bottom', 'bottombar')
+            if ('' !== defaultConfig.layout.bottom) {
+                await expect(bottombar).toBeAttached()
+            } else {
+                await expect(bottombar).not.toBeAttached()
+            }
+        })
+
+        test('table bottom bar is rendering correct child elements as passed with options', async () => {
+            let bottombar = await locateElement(page, '.dataTable-bottom', 'bottombar')
+
+            let bottomLayout = defaultConfig.layout.bottom
+            let bottomLayoutArr = bottomLayout.replaceAll('}{', ' ').replace(/{|}/g, '').split(' ')
+            for (let i = 0; i < bottomLayoutArr.length; i++) {
+                await expect(await locateElement(bottombar, `[data-testid = ${bottomLayoutArr[i]}]`)).toBeAttached()
+            }
+        })
+
     })
 
-    test('is searchbar present when searchale is true and placeholder is correct', async function () {
-        let searchBar = await locateElement(page, '.dataTable-input')
-        if (defaultConfig.searchable) {
-            await expect(searchBar).toBeAttached() // assert whether the element is visible
-            await expect(await (searchBar.getAttribute('placeholder'))).toEqual(defaultConfig.labels.placeholder) // to assert the placeholder
-        } else {
-            await expect(searchBar).not.toBeAttached() // assert the input is not in existence
-        }
-    })
+    test.describe.serial('match config with UI', async () => {
+        // test.describe.configure({ mode: 'serial' });
+        test.beforeAll(async ({ browser }) => {
+            let context = await browser.newContext()
+            page = await context.newPage()
+        })
 
-})
+        test.beforeEach(async () => {
+            await page.goto(testConfigs.url)
+        })
 
-test.describe.serial('tests for bottom bar', async ()=>{
+        test.afterAll(async ({ browser }) => {
+            browser.close()
+        })
 
-    test.beforeAll(async ({ browser }) => {
-        let context = await browser.newContext()
-        page = await context.newPage()
-    })
+        test('are headers being displayed', async () => {
+            let headers = await locateElement(page, '#table thead')
+            if (defaultConfig.header) {
+                await expect(headers).toBeAttached()
+            } else {
+                await expect(headers).not.toBeAttached()
+            }
+        })
 
-    test.beforeEach(async () => {
-        await page.goto(testConfigs.url)
-    })
+        test('is info text correct', async () => {
+            let infoLabel = await locateElement(page, '.dataTable-info')
+            let tr = await page.$$('#table tbody tr')
+            await expect(await (infoLabel.textContent())).toEqual(`Showing 1 to ${tr.length} of 30 entries`)
+        })
 
-    test.afterAll(async ({ browser }) => {
-        browser.close()
-    })
+        test('No entries found', async () => {
+            let searchBar = await locateElement(page, '.dataTable-input')
+            await searchBar.fill('jfejfkl') // Intentionally wrong value so that the search breaks
+            await searchBar.press('Enter')
+            let emptyTable = await locateElement(page, '.dataTables-empty')
+            await expect(await (emptyTable.textContent())).toEqual(defaultConfig.labels.noRows)
+        })
 
+        test('is initial rows equal to perpage', async () => {
+            let tr = await page.$$('#table tbody tr')
+            await expect(await (tr.length)).toEqual(defaultConfig.perPage)
+        })
 
-    test('table bottom bar is only rendering when layout is present', async () => {
-        await locateElement(page, '.dataTable-bottom', 'bottombar')
-        if (!defaultConfig.layout) {
-            await expect(useables.bottombar).not.toBeAttached()
-        } else {
-            await expect(useables.bottombar).toBeAttached()
-        }
-    })
-
-    test('table bottom bar is only rendering when layout.bottom is non empty', async () => {
-        await locateElement(page, '.dataTable-bottom', 'bottombar')
-        if ('' !== defaultConfig.layout.bottom) {
-            await expect(useables.bottombar).toBeAttached()
-        } else {
-            await expect(useables.bottombar).not.toBeAttached()
-        }
-    })
-
-    test('table bottom bar is rendering correct child elements as passed with options', async () => {
-        await locateElement(page, '.dataTable-bottom', 'bottombar')
-
-        let bottomLayout = defaultConfig.layout.bottom
-        let bottomLayoutArr = bottomLayout.replaceAll('}{', ' ').replace(/{|}/g, '').split(' ')
-        for (let i = 0; i < bottomLayoutArr.length; i++) {
-            await expect(await locateElement(useables.bottombar, `[data-testid = ${bottomLayoutArr[i]}]`)).toBeAttached()
-        }
-    })
-
-})
-
-test.describe.serial('match config with UI', async () => {
-    // test.describe.configure({ mode: 'serial' });
-    test.beforeAll(async ({ browser }) => {
-        let context = await browser.newContext()
-        page = await context.newPage()
-    })
-
-    test.beforeEach(async () => {
-        await page.goto(testConfigs.url)
-    })
-
-    test.afterAll(async ({ browser }) => {
-        browser.close()
-    })
-
-    test('are headers being displayed', async () => {
-        let headers = await locateElement(page, '#table thead')
-        if (defaultConfig.header) {
-            await expect(headers).toBeAttached()
-        } else {
-            await expect(headers).not.toBeAttached()
-        }
-    })
-
-    test('is info text correct', async () => {
-        let infoLabel = await locateElement(page, '.dataTable-info')
-        let tr = await page.$$('#table tbody tr')
-        await expect(await (infoLabel.textContent())).toEqual(`Showing 1 to ${tr.length} of 30 entries`)
-    })
-
-    test('No entries found', async () => {
-        let searchBar = await locateElement(page, '.dataTable-input')
-        await searchBar.fill('jfejfkl') // Intentionally wrong value so that the search breaks
-        await searchBar.press('Enter')
-        let emptyTable = await locateElement(page, '.dataTables-empty')
-        await expect(await (emptyTable.textContent())).toEqual(defaultConfig.labels.noRows)
-    })
-
-    test('is initial rows equal to perpage', async () => {
-        let tr = await page.$$('#table tbody tr')
-        await expect(await (tr.length)).toEqual(defaultConfig.perPage)
     })
 
 })
